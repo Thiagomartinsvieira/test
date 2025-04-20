4131c623cb92d72a8bebbf462107178c6a3a6b73
Thiago <thiago.devstack@gmail.com>
2025-04-20 09:41:24 -0300

    test: query methods

---
+++ bytebank/src/componentes/Menu/menu.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react"
+import Menu from "./index"
+
+test("To be render a link to the initial page", () => {
+    render(<Menu/>);
+    const linkInitialPage = screen.getByText('Inicial')
+    expect(linkInitialPage).toBeInTheDocument();
+})
+
+test("To be render a list of links", () => {
+    render(<Menu/>);
+    const listOfLinks = screen.getAllByRole('link')
+    expect(listOfLinks).toHaveLength(4);
+})
+
+test("Dont't have to render a link to extract", () => {
+    render(<Menu/>);
+    const linkExtract = screen.queryByTestId('Extract')
+    expect(linkExtract).not.toBeInTheDocument();
+})
