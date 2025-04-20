3c9d93b091948765e53dca5ff13b45a1a6f6db3f
Thiago <thiago.devstack@gmail.com>
2025-04-20 09:52:33 -0300

    test: Snapshot tests

--- bytebank/src/componentes/Menu/menu.test.js
+++ bytebank/src/componentes/Menu/menu.test.js
@@ -1,20 +1,27 @@
-import { render, screen } from "@testing-library/react"
-import Menu from "./index"
+import { render, screen } from '@testing-library/react'
+import Menu from './index'
 
-test("To be render a link to the initial page", () => {
-    render(<Menu/>);
+test('To be render a link to the initial page', () => {
+    render(<Menu />)
     const linkInitialPage = screen.getByText('Inicial')
-    expect(linkInitialPage).toBeInTheDocument();
+    expect(linkInitialPage).toBeInTheDocument()
 })
 
-test("To be render a list of links", () => {
-    render(<Menu/>);
+test('To be render a list of links', () => {
+    render(<Menu />)
     const listOfLinks = screen.getAllByRole('link')
-    expect(listOfLinks).toHaveLength(4);
+    expect(listOfLinks).toHaveLength(4)
 })
 
 test("Dont't have to render a link to extract", () => {
-    render(<Menu/>);
+    render(<Menu />)
     const linkExtract = screen.queryByTestId('Extract')
-    expect(linkExtract).not.toBeInTheDocument();
+    expect(linkExtract).not.toBeInTheDocument()
+})
+
+test("To be render a list of links with a class link", () => {
+    render(<Menu/>)
+    const links = screen.getAllByRole("link")
+    links.forEach((link) => expect(link).toHaveClass('links'));
+    expect(links).toMatchSnapshot()
 })
