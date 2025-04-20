8b04e1e3ba985661e33c6e5c70db5a55814413de
Thiago <thiago.devstack@gmail.com>
2025-04-20 09:24:41 -0300

    test: testing a component

--- bytebank/src/componentes/Cabecalho/cabecalho.test.js
+++ bytebank/src/componentes/Cabecalho/cabecalho.test.js
@@ -1,4 +1,8 @@
-test("First test", () => {
-    const number = 10;
-    expect(number).toBe(10);
+import { render, screen } from '@testing-library/react'
+import Cabecalho from './index'
+
+test('To be render the logged user', () => {
+    render(<Cabecalho />)
+    const userName = screen.getByText('Joana Fonseca Gomes')
+    expect(userName).toBeInTheDocument();
 })
