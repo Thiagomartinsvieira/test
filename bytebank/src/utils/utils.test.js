d8c6a1e74f79d0f996adc4e36ce882c7cd542ed7
Thiago Martins Vieira <thiago.devstack@gmail.com>
2025-04-21 21:06:25 -0300

    test: testing a function

--- bytebank/src/utils/utils.test.js
+++ bytebank/src/utils/utils.test.js
@@ -1,3 +1,29 @@
+import {calculaNovoSaldo as calculateNewBalance} from "./index";
+
+describe("When i make a transaction", () => {
+    test('Its a Deposit, the balance must increse', () => {
+        const transaction = {
+            transacao: 'Balance',
+            valor: 50,
+        }
+
+        const newBalance = calculateNewBalance(transaction, 100);
+
+        expect(newBalance).toBe(150);
+    })
+
+    test('Its a transfer, the balance must decresses', () => {
+        const transaction = {
+            transacao: 'transfer',
+            valor: 50,
+        }
+
+        const newBalance = calculateNewBalance(transaction, 100);
+
+        expect(newBalance).toBe(50);
+    })
+})
+
 test("To be return the balance value updated with the income", () => {
     const calculateYield = jest.fn(balance => balance + balance * 0.005)
 
